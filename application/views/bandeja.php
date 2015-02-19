9ad01de43838a998ad90e590b2f505fda42ca03d
EmilioEre <emi.rivero11@gmail.com>
2015-02-19 13:27:05 -0300

    Tercera version: Pagina inicial

---
+++ application/views/bandeja.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<html>
+	<head>
+	<title>Templo MIddas</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("css/bandeja.css") ?>"/>
+	</head>
+	<body>
+	<div id="contenedor"> 
+		<div id="encabezado">
+			<div id="titulo">
+			</div>
+		</div>
+		<div id="cuerpo">
+		</div>
+		<div id="pie">
+			<h2>CHEPES - LA RIOJA</h2>
+		</div>
+	</div>
+	</body>
+</html>
